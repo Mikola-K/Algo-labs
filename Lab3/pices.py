7fec21f107434f71d937713f5d15fa16f58d2fa6
Mikola-K <hotoneshot.ua@gmail.com>
2021-11-26 10:05:22 +0200

    fix code for lab

--- Lab3/pices.py
+++ Lab3/pices.py
@@ -1,28 +1,31 @@
 from math import ceil
 
-def can_eat_in_time(piles, hour, speed):
+def can_eat_in_time(piles, hours, eating_speed):
     required_time = 0
     for pile in piles:
-        required_time += ceil(pile/speed)
-    return required_time <= hour
+        required_time += ceil(pile/eating_speed)
+    return required_time <= hours
 
-def count_min_eating_time(piles, hour):
-    biggest_pile = max(piles)
-    num_of_piles = len (piles)
-    if num_of_piles == hour:
+def find_min_eating_speed(piles, hours, possible_eating_speeds):
+    if len(possible_eating_speeds) == 1:
+        return possible_eating_speeds[0]
+    
+    current_eating_speed = possible_eating_speeds [len(possible_eating_speeds) // 2]
+    if can_eat_in_time (piles, hours, current_eating_speed):
+        return find_min_eating_speed(piles,hours, possible_eating_speeds[len(possible_eating_speeds) // 2:])
+    else:
+        return find_min_eating_speed(piles,hours, possible_eating_speeds[:len(possible_eating_speeds) // 2])
+
+def count_min_eating_time(piles, hours):
+    if len(piles) == hours:
         return max(piles)
-    if num_of_piles > hour:
+    if len(piles) > hours:
         raise ValueError("Jackie will not be able to eat all the bananas in time")
-    if sum(piles) <= hour:
+    if sum(piles) <= hours:
         return 1
-    average_eating_time = sum(piles) // hour
-    while average_eating_time < biggest_pile: 
-        speed = (average_eating_time + biggest_pile) // 2
-        if can_eat_in_time(piles, hour, speed):
-            biggest_pile = speed
-        else:
-            average_eating_time = speed + 1
-    return average_eating_time
+    
+    possible_eating_speeds = [i for i in range(max(piles), 1, -1)]
+    return find_min_eating_speed(piles, hours, possible_eating_speeds)
 
 if __name__ == '__main__':
     print(count_min_eating_time([30,11,23,4,20],6))
